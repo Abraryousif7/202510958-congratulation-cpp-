6d761477a6edbeded63587a34b84743ff8b3accf
Abraryousif7 <abraryou208@gmail.com>
2026-05-14 14:07:56 +0200

    Create     void_function.cpp

---
+++ void_function.cpp
@@ -0,0 +1,15 @@
+// Question 5
+#include <iostream>
+using namespace std;
+
+void message(string name)
+{
+    cout << "Hello " << name << endl;
+}
+
+int main()
+{
+    message("Abrar");
+
+    return 0;
+}
